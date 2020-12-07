46e0d8fc62d38725316a32de38451fd50fbd524b
Cage Johnson <cagejohnson@Admins-iMac.local>
2020-12-06 19:00:02 -0600

    merging the changes from laptop

--- user-interaction-service/main.go
+++ user-interaction-service/main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
+	"net/http"
+
+	"./models"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/kabukky/httpscerts"
 	_ "github.com/mattn/go-sqlite3"
-	"./models"
-	"encoding/json"
 )
 
 func main() {
 
 	models.OpenDatabaseConnection("./studier.db")
 
-	http.HandleFunc("/content", func(w http.ResponseWriter,r *http.Request){
-		enableCors(&w)
+	r := mux.NewRouter()
+	r.HandleFunc("/content", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Print("Content Route")
+		enableCors(&writer)
 		probs, _ := models.AllProblems()
 		v, _ := json.Marshal(probs)
-		io.WriteString(w, string(v))
+		io.WriteString(writer, string(v))
 	})
-	startServerTLS()
-}
 
+	r.HandleFunc("/problem/{id}", func(writer http.ResponseWriter, request *http.Request) {
+
+		vars := mux.Vars(request)
+		id := vars["id"]
+		fmt.Printf(request.URL.Path, id)
+		enableCors(&writer)
+		// probs, _ := models.AllProblems()
+		// v, _ := json.Marshal(probs)
+		// io.WriteString(w, string(v))
+	})
+
+	startServerTLS(r)
+}
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
@@ -34,19 +50,28 @@ func enableCors(w *http.ResponseWriter) {
 
 }
 
-
-func startServerTLS(){
+func startServerTLS(r *mux.Router) {
 	// Check if the cert files are available.
 	err := httpscerts.Check("cert.pem", "key.pem")
 	// If they are not available, generate new ones.
 	if err != nil {
-		err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:8090")
+		err = httpscerts.Generate("cert.pem", "key.pem", "localhost:8090")
 		if err != nil {
 			log.Fatal("Error: Couldn't create https certs.")
 		}
 	}
 
-	http.ListenAndServeTLS(":8090", "cert.pem", "key.pem", nil)
+	fmt.Print("about to serve")
+
+	// srv := &http.Server{
+	// 	Handler: r,
+	// 	Addr:    "localhost:8090",
+	// 	// Good practice: enforce timeouts for servers you create!
+	// 	WriteTimeout: 15 * time.Second,
+	// 	ReadTimeout:  15 * time.Second,
+	// }
+
+	// log.Fatal(srv.ListenAndServe())
+
+	http.ListenAndServeTLS(":8090", "cert.pem", "key.pem", r)
 }
-
-
